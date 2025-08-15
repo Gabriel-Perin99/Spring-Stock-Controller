2ab515c4122ac540fee82fc746a5e04c3b4b529a
Gabriel Perin <gabriel.perin@tramontina.com>
2025-08-15 16:38:47 -0300

    Created ProductService and ProductContoller

---
+++ src/main/java/com/gitHub/GabriePerin/Spring_Stock_Controller/controller/ProductController.java
@@ -0,0 +1,38 @@
+package com.gitHub.GabriePerin.Spring_Stock_Controller.controller;
+
+import com.gitHub.GabriePerin.Spring_Stock_Controller.model.Product;
+import com.gitHub.GabriePerin.Spring_Stock_Controller.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+//This class deals with HTTP requests and returns the appropriate response to each client.
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public List<Product> listAll(){
+        return productService.listAll();
+    }
+
+    @PostMapping
+    public Product save(@RequestBody Product product){
+        return productService.save(product);
+    }
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id){
+        productService.delete(id);
+    }
+    @PutMapping("/{id}")
+    public Product update (@PathVariable Long id, @RequestBody Product product){
+        return productService.update(id, product);
+    }
+    @GetMapping("/{id}")
+    public Optional<Product> findById(@PathVariable Long id){
+        return productService.findById(id);
+    }
+}
---
+++ src/main/java/com/gitHub/GabriePerin/Spring_Stock_Controller/service/ProductService.java
@@ -0,0 +1,36 @@
+package com.gitHub.GabriePerin.Spring_Stock_Controller.service;
+
+import com.gitHub.GabriePerin.Spring_Stock_Controller.model.Product;
+import com.gitHub.GabriePerin.Spring_Stock_Controller.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+//This class aims to perform CRUD operations, which were once done manually, automatically and internal
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> listAll(){
+        return productRepository.findAll();
+    }
+    public Product save (Product product){
+        return productRepository.save(product);
+    }
+    public void delete(Long id){
+        productRepository.deleteById(id);
+    }
+    public Product update(long id, Product product){
+        if(productRepository.existsById(id)){
+            product.setId(id);
+            return productRepository.save(product);
+        }else{
+            throw new RuntimeException("Não foi encontrado nenhum produto");
+        }
+    }
+    public Optional<Product> findById(Long id){
+        return productRepository.findById(id);
+    }
+}
