50bedd6e89d39496c1bb14a9194689b9169e23a1
Gabriel Perin <gabriel.perin@tramontina.com>
2025-08-15 11:37:14 -0300

    Base Structure | Pom and Application Configured

---
+++ src/main/java/com/gitHub/GabriePerin/Spring_Stock_Controller/SpringStockControllerApplication.java
@@ -0,0 +1,13 @@
+package com.gitHub.GabriePerin.Spring_Stock_Controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStockControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStockControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitHub/GabriePerin/Spring_Stock_Controller/SpringStockControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitHub.GabriePerin.Spring_Stock_Controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStockControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
