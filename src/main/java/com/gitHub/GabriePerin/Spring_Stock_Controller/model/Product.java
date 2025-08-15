f36e7e672c97795d8887b7461ea53aaa5e3a1855
Gabriel Perin <gabriel.perin@tramontina.com>
2025-08-15 15:59:43 -0300

    Created Classes for Model and Repository | Adjusted dependencies of the pom.xml

---
+++ src/main/java/com/gitHub/GabriePerin/Spring_Stock_Controller/model/Product.java
@@ -0,0 +1,69 @@
+package com.gitHub.GabriePerin.Spring_Stock_Controller.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+
+@Entity
+public class Product {
+    //Spring will automatically generate an ID in the database..
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    //Don't let the name be null
+    @NotEmpty(message = "Informe um nome!")
+    private String name;
+
+    private int quantity;
+    private double price;
+    private String status;
+
+    public Product(String name, int quantity, double price,String status){
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+        this.status = status;
+    }
+    //Empty Constructor
+    public Product(){}
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id){
+        this.id = id;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public int getQuantity(){
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
+
---
+++ src/main/java/com/gitHub/GabriePerin/Spring_Stock_Controller/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.gitHub.GabriePerin.Spring_Stock_Controller.repository;
+
+import com.gitHub.GabriePerin.Spring_Stock_Controller.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//// This class is generic and, due to Spring, will manage the Model Product class, performing CRUD BASIC OPERATIONS
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
